79c6690e1234468de67b3671d3bbb63e9e197ab8
bhaskar4k <bhaskarndy@gmail.com>
2025-08-24 02:02:33 +0530

    Updated reg form

--- LOCK-IN-ONE.APP/src/app/page-component/register-org/register-org.component.ts
+++ LOCK-IN-ONE.APP/src/app/page-component/register-org/register-org.component.ts
@@ -1,12 +1,74 @@
-import { Component } from '@angular/core';
+import { StepperSelectionEvent } from '@angular/cdk/stepper';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener, inject, OnInit } from '@angular/core';
+import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatStepperModule } from '@angular/material/stepper';
+
 
 @Component({
   selector: 'app-register-org',
-  imports: [],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatStepperModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+  ],
   standalone: true,
   templateUrl: './register-org.component.html',
   styleUrl: './register-org.component.css'
 })
-export class RegisterOrgComponent {
+export class RegisterOrgComponent implements OnInit {
+  private _formBuilder = inject(FormBuilder);
 
+  selectedStepIndex: number = 0;
+  isLinear = false;
+  stepperOrientation: 'horizontal' | 'vertical' = 'horizontal';
+
+  org_name: string | null = null;
+  org_email: string | null = null;
+
+  application_count: number | null = 0;
+  applications: string[] = [];
+
+  ngOnInit() {
+    this.setStepperOrientation();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.setStepperOrientation();
+  }
+
+  setStepperOrientation() {
+    this.stepperOrientation = window.innerWidth < 1200 ? 'vertical' : 'horizontal';
+  }
+
+  firstFormGroup = this._formBuilder.group({
+    firstCtrl: ['', Validators.required],
+  });
+  secondFormGroup = this._formBuilder.group({
+    secondCtrl: ['', Validators.required],
+  });
+
+  onStepChange(event: StepperSelectionEvent): void {
+    this.selectedStepIndex = event.selectedIndex;
+  }
+
+
+  UpdateApplications() {
+    const count = this.application_count || 0;
+
+    // Resize the array to match the count
+    if (count > this.applications.length) {
+      // Add empty strings if count increased
+      this.applications.push(...Array(count - this.applications.length).fill(''));
+    } else if (count < this.applications.length) {
+      // Remove extra elements if count decreased
+      this.applications.splice(count);
+    }
+  }
 }
