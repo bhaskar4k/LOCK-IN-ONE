2e5a36ea775ef22ac91a16bc2ceff614dc063539
bhaskar4k <bhaskarndy@gmail.com>
2025-08-24 18:22:09 +0530

    Login added

--- LOCK-IN-ONE.APP/src/app/app.routes.ts
+++ LOCK-IN-ONE.APP/src/app/app.routes.ts
@@ -2,10 +2,13 @@ import { Routes } from "@angular/router";
 import { HomeComponent } from "./page-component/home/home.component";
 import { RegisterOrgComponent } from "./page-component/register-org/register-org.component";
 import { ManageOrgComponent } from "./page-component/manage-org/manage-org.component";
+import { LoginComponent } from "./page-component/login/login.component";
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'home', component: HomeComponent },
+
+    { path: 'login', component: LoginComponent },
 
     { path: 'organization/register', component: RegisterOrgComponent },
     { path: 'organization/manage', component: ManageOrgComponent },
