b41199c34cf649407751b59ba002adfec91e79d4
bhaskar4k <bhaskarndy@gmail.com>
2025-08-24 18:09:03 +0530

    Manage Org component

--- LOCK-IN-ONE.APP/src/app/app.routes.ts
+++ LOCK-IN-ONE.APP/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from "@angular/router";
 import { HomeComponent } from "./page-component/home/home.component";
 import { RegisterOrgComponent } from "./page-component/register-org/register-org.component";
+import { ManageOrgComponent } from "./page-component/manage-org/manage-org.component";
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'home', component: HomeComponent },
 
-    { path: 'register-your-org', component: RegisterOrgComponent },
+    { path: 'organization/register', component: RegisterOrgComponent },
+    { path: 'organization/manage', component: ManageOrgComponent },
 ];
---
+++ LOCK-IN-ONE.APP/src/app/page-component/manage-org/manage-org.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-org',
+  imports: [],
+  templateUrl: './manage-org.component.html',
+  styleUrl: './manage-org.component.css'
+})
+export class ManageOrgComponent {
+
+}
