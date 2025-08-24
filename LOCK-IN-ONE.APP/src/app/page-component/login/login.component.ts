7993aedb539fa4149af25dc829918e25da5fd403
bhaskar4k <bhaskarndy@gmail.com>
2025-08-24 19:20:32 +0530

    Login done

--- LOCK-IN-ONE.APP/src/app/page-component/login/login.component.ts
+++ LOCK-IN-ONE.APP/src/app/page-component/login/login.component.ts
@@ -5,10 +5,11 @@ import { ResponseType, TrueFalse } from '../../common-constants/enum-constants';
 import { OrganizationService } from '../../service/organization/organization.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 @Component({
   selector: 'app-login',
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, MatProgressBarModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
@@ -21,43 +22,47 @@ export class LoginComponent {
   email: string | null = null;
   password: string | null = null;
 
+  ValidateForm() {
+    return this.email !== null && this.email.trim() !== '' && this.password !== null && this.password.trim() !== '';
+  }
+
   Login() {
-      const obj = {
-        email: this.email,
-        password: this.password,
-      };
-  
-      this.matProgressBarVisible = true;
-  
-      this.organizationService.DoLogin(obj).subscribe({
-        next: async (response) => {
-          this.matProgressBarVisible = false;
-  
-          if (response && response.success === TrueFalse.TRUE) {
-            this.OpenDialog(response.message, ResponseType.SUCCESS, false);
-          } else {
-            this.OpenDialog(response.message, ResponseType.ERROR, false);
-          }
-        },
-        error: (err) => {
-          this.matProgressBarVisible = false;
-          this.OpenDialog(err.error.message ?? "Failed to process the request!", ResponseType.ERROR, false);
+    const obj = {
+      email: this.email,
+      password: this.password,
+    };
+
+    this.matProgressBarVisible = true;
+
+    this.organizationService.DoLogin(obj).subscribe({
+      next: async (response) => {
+        this.matProgressBarVisible = false;
+
+        if (response && response.success === TrueFalse.TRUE) {
+          this.OpenDialog(response.message, ResponseType.SUCCESS, false);
+        } else {
+          this.OpenDialog(response.message, ResponseType.ERROR, false);
         }
-      });
-    }
-  
-    OpenDialog(dialogText: string, dialogType: string, pageReloadNeeded: boolean): void {
-      const dialogRef = this.dialog.open(CustomAlertComponent, {
-        width: '30rem',
-        height: 'max-content',
-        disableClose: true,
-        data: { text: dialogText, type: dialogType }
-      });
-  
-      dialogRef.afterClosed().subscribe(() => {
-        if (pageReloadNeeded) {
-          window.location.reload();
-        }
-      });
-    }
+      },
+      error: (err) => {
+        this.matProgressBarVisible = false;
+        this.OpenDialog(err.error.message ?? "Failed to process the request!", ResponseType.ERROR, false);
+      }
+    });
+  }
+
+  OpenDialog(dialogText: string, dialogType: string, pageReloadNeeded: boolean): void {
+    const dialogRef = this.dialog.open(CustomAlertComponent, {
+      width: '30rem',
+      height: 'max-content',
+      disableClose: true,
+      data: { text: dialogText, type: dialogType }
+    });
+
+    dialogRef.afterClosed().subscribe(() => {
+      if (pageReloadNeeded) {
+        window.location.reload();
+      }
+    });
+  }
 }
