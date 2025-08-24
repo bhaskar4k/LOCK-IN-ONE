e7629981311b3fac8c49ad341f142449d86bd22a
bhaskar4k <bhaskarndy@gmail.com>
2025-08-24 12:29:09 +0530

    Setup done

--- LOCK-IN-ONE.API/src/helper/Routes.js
+++ LOCK-IN-ONE.API/src/helper/Routes.js
@@ -1,7 +1,9 @@
 import OrganizationRoutes from '../routes/Organization.route.js';
 
 const InitializeRoutes = (app) => {
+    console.log("Trying to initialize routes!");
     app.use("/organization", OrganizationRoutes);
+    console.log("Routes initialized successfully!\n");
 };
 
 export default InitializeRoutes;
